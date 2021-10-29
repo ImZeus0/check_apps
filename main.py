afed5ff57f15af7046108c56e73bf359cf35d08a
ImZeus0 <gyber25@gmail.com>
2021-10-29 15:40:21 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+import time
+import requests
+
+URL_SOURCE = 'https://apps.lukashenkopresidentnet.online/bot.php?zenkRequest=get_all_apps&secret=~6KBsKMPhS2Y'
+BASE_URL = 'http://127.0.0.1:3458/api'
+START_APP = '898274564163561'
+
+
+def get_source():
+    data = []
+    response = requests.get(url=URL_SOURCE, verify=False)
+    response = response.text.split(';')
+    if len(response) == 1:
+        return None
+    for res in response:
+        obj_dict = res.split(',')
+        obj = {}
+        obj['name'] = obj_dict[0]
+        obj['id'] = obj_dict[1]
+        data.append(obj)
+    return data
+
+
+def get_all_apps():
+    responce = requests.get(BASE_URL + '/all_apps')
+    if responce.status_code == 200:
+        return responce.json()
+    return None
+
+def send_state_in_account(state):
+    for app in state:
+        headers = {'Content-Type': 'application/json'}
+        responce = requests.post(f'{BASE_URL}/send_ids',headers=headers,json=app,verify=False)
+        print(responce.json())
+
+def get_current_state_apps():
+    apps = get_all_apps()
+    data = []
+    for app in apps:
+        url = f'{BASE_URL}/get_ids?id_app={str(app["id"])}'
+        res = requests.get(url)
+        data.append(res.json())
+        time.sleep(3)
+    return data
+
+def reload_local_state_apps():
+    state = get_current_state_apps()
+    with open('state_apps.json','w') as writer:
+        writer.write(str(state).replace("'",'"'))
+
+def update_local_state_apps(state):
+    with open('state_apps.json','w') as writer:
+        writer.write(str(state).replace("'",'"'))
+
+def local_state():
+    with open('state_apps.json','r') as reader:
+        data = json.loads(reader.read())
+    return data
+
+if __name__ == '__main__':
+    while 1:
+        local_state = local_state()
+        new_state = get_source()
+        #new_state = [{'name':'Pirate Slots','id':'390685585862216'},{'name':'Pirate Slots','id':'2950120141966355'},{'name':'Pirate Slots','id':'1097061317766214'},{'name':'test','id':'260502292693212'}]
+        if new_state == None:
+            print('Not update')
+            exit()
+
+        for new_app in new_state:
+            for local_app in local_state:
+                if new_app['name'] == local_app['appname']:
+                    tmp = local_app['data']
+                    tmp.append(new_app['id'])
+                    tmp = set(tmp)
+                    local_app['data'] = list(tmp)
+
+        send_state_in_account(local_state)
+        update_local_state_apps(local_state)
+        time.sleep(3600)
+
+
